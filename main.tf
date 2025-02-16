458cd8af291f6c780a329dad77088cfe2198cd7e
Andrew Kemp-Dahlberg <Andrew.KempDahlberg@UN-MAC-Andrew.KempDahlberg>
2025-02-16 01:18:02 -0600

    added okta signon policy and singon policy rules. Just a progress save. Need to convert to var with list of objects and use the count method. We also have to add a specific stepup auth policy for admin group

--- main.tf
+++ main.tf
@@ -1,4 +1,121 @@
 locals {
+  authentication_policy_name = ${}
+}
+resource "okta_app_signon_policy" "authentication_policy" {
+  description = "Default policy that requir"
+  name        = "Any two factors"
+}
+
+resource "okta_app_signon_policy_rule" "authentication_policy_rule" {
+  access                      = "ALLOW"
+  constraints                 = ["{\"possession\":{\"required\":true,\"hardwareProtection\":\"REQUIRED\",\"userPresence\":\"REQUIRED\",\"userVerification\":\"REQUIRED\"}}"]
+  custom_expression           = null
+  device_assurances_included  = null
+  device_is_managed           = null
+  device_is_registered        = null
+  factor_mode                 = "2FA"
+  groups_excluded             = []
+  groups_included             = ["00g11p7vbcqI3vXBt2p8"]
+  inactivity_period           = null
+  name                        = "Mac and Windows Devices"
+  network_connection          = "ANYWHERE"
+  network_excludes            = null
+  network_includes            = null
+  policy_id                   = okta_app_signon_policy.authentication_policy.id
+  priority                    = 3
+  re_authentication_frequency = "PT43800H"
+  risk_score                  = "ANY"
+  status                      = "ACTIVE"
+  type                        = "ASSURANCE"
+  user_types_excluded         = []
+  user_types_included         = []
+  users_excluded              = []
+  users_included              = []
+  platform_include {
+    os_expression = null
+    os_type       = "MACOS"
+    type          = "DESKTOP"
+  }
+  platform_include {
+    os_expression = null
+    os_type       = "WINDOWS"
+    type          = "DESKTOP"
+  }
+}
+
+resource "okta_app_signon_policy_rule" "authentication_policy_rule" {
+  access                      = "ALLOW"
+  constraints                 = ["{\"knowledge\":{\"required\":false},\"possession\":{\"authenticationMethods\":[{\"key\":\"okta_verify\",\"method\":\"signed_nonce\"}],\"required\":false,\"hardwareProtection\":\"REQUIRED\",\"phishingResistant\":\"REQUIRED\",\"userPresence\":\"REQUIRED\"}}"]
+  custom_expression           = null
+  device_assurances_included  = ["daeya6jtpsaMCFM4h2p7", "daeya6odzfBCEPM8F2p7"]
+  device_is_managed           = false
+  device_is_registered        = false
+  factor_mode                 = "2FA"
+  groups_excluded             = []
+  groups_included             = ["00g11p7vbcqI3vXBt2p8"]
+  inactivity_period           = null
+  name                        = "Android and iOS devices"
+  network_connection          = "ANYWHERE"
+  network_excludes            = null
+  network_includes            = null
+  policy_id                   = "rsto89gt30Dn9uLiy2p7"
+  priority                    = 4
+  re_authentication_frequency = "PT43800H"
+  risk_score                  = "ANY"
+  status                      = "ACTIVE"
+  type                        = "ASSURANCE"
+  user_types_excluded         = []
+  user_types_included         = []
+  users_excluded              = []
+  users_included              = []
+}
+
+resource "okta_app_signon_policy_rule" "authentication_policy_rule" {
+  access                      = "ALLOW"
+  constraints                 = ["{\"knowledge\":{\"reauthenticateIn\":\"PT43800H\",\"types\":[\"password\"],\"required\":true},\"possession\":{\"required\":true,\"hardwareProtection\":\"REQUIRED\",\"userPresence\":\"REQUIRED\"}}"]
+  custom_expression           = null
+  device_assurances_included  = null
+  device_is_managed           = null
+  device_is_registered        = null
+  factor_mode                 = "2FA"
+  groups_excluded             = null
+  groups_included             = null
+  inactivity_period           = null
+  name                        = "Unsupported Devices"
+  network_connection          = "ANYWHERE"
+  network_excludes            = null
+  network_includes            = null
+  policy_id                   = "rsto89gt30Dn9uLiy2p7"
+  priority                    = 6
+  re_authentication_frequency = "PT43800H"
+  risk_score                  = "ANY"
+  status                      = "ACTIVE"
+  type                        = "ASSURANCE"
+  user_types_excluded         = []
+  user_types_included         = []
+  users_excluded              = []
+  users_included              = []
+  platform_include {
+    os_expression = null
+    os_type       = "CHROMEOS"
+    type          = "DESKTOP"
+  }
+  platform_include {
+    os_expression = null
+    os_type       = "OTHER"
+    type          = "DESKTOP"
+  }
+  platform_include {
+    os_expression = null
+    os_type       = "OTHER"
+    type          = "MOBILE"
+  }
+}
+
+
+
+
+locals {
   app_settings = {
     accessibility_error_redirect_url = var.accessibility_error_redirect_url
     accessibility_login_redirect_url = var.accessibility_login_redirect_url
@@ -7,7 +124,7 @@ locals {
     admin_note                       = var.admin_note
     assertion_signed                 = var.assertion_signed
     audience                         = var.audience
-    authentication_policy            = data.okta_policy.access.id
+    authentication_policy            = okta_app_signon_policy.authentication_policy.id
     authn_context_class_ref          = var.authn_context_class_ref
     auto_submit_toolbar              = var.auto_submit_toolbar
     default_relay_state              = var.default_relay_state
