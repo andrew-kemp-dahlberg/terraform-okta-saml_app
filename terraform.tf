490d5a6126d2c9750f2d3a4380da41b515ed9ead
Andrew Kemp-Dahlberg <akempdah@gmail.com>
2025-03-07 19:06:22 -0600

    put all environmental vars into an object for cleanliness when calling the module

--- terraform.tf
+++ terraform.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "okta" {
-  org_name       = var.org_name
-  base_url       = var.base_url
-  client_id      = var.client_id
-  private_key_id = var.private_key_id
-  private_key    = var.private_key
+  org_name       = var.environment.org_name
+  base_url       = var.environment.base_url
+  client_id      = var.environment.client_id
+  private_key_id = var.environment.private_key_id
+  private_key    = var.environment.private_key
   scopes         = ["okta.apps.manage", "okta.apps.read", "okta.groups.manage", "okta.groups.read", "okta.policies.manage", "okta.policies.read", "okta.profileMappings.manage", "okta.profileMappings.read"]
 }
