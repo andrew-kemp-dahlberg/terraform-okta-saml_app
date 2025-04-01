be6aea791964b71cc9a66d96ae1afc421b09cd42
Andrew Kemp-Dahlberg <akempdah@gmail.com>
2025-04-01 09:42:32 -0500

    clean

--- main.tf
+++ main.tf
@@ -206,11 +206,6 @@ data "http" "saml_app_list" {
     Accept = "application/json"
     Authorization = "SSWS ${var.environment.api_token}"
   }
-  lifecycle {
-    postcondition {
-    condition = data.http.saml_app_list.status_code == 200
-    error_message = "API request failed with status code: ${data.http.saml_app_list.status_code}. Error: ${data.http.saml_app_list.response_body}"
-  }
 }
 
 locals {
@@ -227,19 +222,8 @@ data "http" "schema" {
     Accept = "application/json"
     Authorization = "SSWS ${var.environment.api_token}"
   }
-  lifecycle {
-     precondition {
-      condition = data.http.saml_app_list.status_code == 200
-      error_message = "API request failed with status code: ${data.http.saml_app_list.status_code}. Error: ${data.http.saml_app_list.response_body}"
-    }
 
-    postcondition {
-      condition = data.http.schema.status_code == 200
-      error_message = "Schema API request failed with status code: ${data.http.schema.status_code}. Error: ${data.http.schema.response_body}"
-    }
-  }
 }
-
 
 data "external" "pre-condition" {
   program = ["bash", "-c", <<-EOT
@@ -249,13 +233,21 @@ data "external" "pre-condition" {
 
   lifecycle {
     # Check SAML app list API response
- 
+    precondition {
+      condition = data.http.saml_app_list.status_code == 200
+      error_message = "API request failed with status code: ${data.http.saml_app_list.status_code}. Error: ${data.http.saml_app_list.response_body}"
+    }
 
     # Check SAML app ID
     precondition {
       condition = local.saml_app_id == "none" || local.saml_app_id == try(okta_app_saml.saml_app.id, "n/a")
       error_message = "An application with label '${local.saml_label}' already exists in Okta outside of Terraform. Either modify the label in your configuration or delete/rename the existing application in Okta."
-      }
+    }
+
+    # Check schema API response
+    precondition {
+      condition = data.http.schema.status_code == 200
+      error_message = "Schema API request failed with status code: ${data.http.schema.status_code}. Error: ${data.http.schema.response_body}"
     }
   }
 }
@@ -302,7 +294,6 @@ locals {
     type        = "string"
     user_type   = "default"
   }] : var.base_schema
-}
 }
 
 resource "okta_app_user_base_schema_property" "properties" {
